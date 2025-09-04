af098a973fd7a132a33715c8766c4ec67e6d9050
devilboi95 <turnickii.n@gmail>
2025-09-04 15:10:34 +0500

    теперь всё в абстактном классе, логика всех контекстов в общих чертах идентчна

--- CookingBot/Application/Commands/AddRecipe/Flow/RecipeContext.cs
+++ CookingBot/Application/Commands/AddRecipe/Flow/RecipeContext.cs
@@ -8,50 +8,13 @@ namespace CookingBot.Application.Commands.AddRecipe.Flow;
 
 public class RecipeContext<TPayload, TState> : IDetailContext<TPayload, TState> where TState : Enum
 {
-    private readonly ITransactionService _transactionService;
-    public bool WasChanged { get; private set; }
-
-    public TState State => (TState)(object)ChatContext.State;
-
-    [field: AllowNull, MaybeNull]
-    public TPayload Payload
+    private RecipeContext(ChatContext chatContext, TPayload? payload, ITransactionService transactionService) :
+        base(chatContext, payload, transactionService)
     {
-        get
-        {
-            if (field == null) throw new NullReferenceException($"Payload in {typeof(TPayload)} is null");
-
-            return field;
-        }
-        private set;
     }
 
-    private ChatContext ChatContext { get; }
-
-    private RecipeContext(ChatContext chatContext, TPayload? payload,
-        ITransactionService transactionService)
-    {
-        ChatContext = chatContext;
-        Payload = payload;
-        _transactionService = transactionService;
-    }
-
-    public IDetailContext<TPayload, TState> NextState()
-    {
-        _transactionService.NextState(ChatContext);
-        WasChanged = true;
-
-        return this;
-    }
-
-    public IDetailContext<TPayload, TState> PostPayload(TPayload payload)
-    {
-        Payload = payload;
-        ChatContext.Payload = JsonConvert.SerializeObject(payload);
-
-        return this;
-    }
-
-    //todo: как будто лучше выкинуть это в абстрактную фабрику. и тем самым одним extensions можно будет добавлять все элементы flow. Add(typeof(FlodName..
+    
+    //todo: как будто лучше выкинуть это в абстрактную фабрику. и тем самым одним extensions можно будет добавлять все элементы flow. Add(typeof(FlodName. или пока оставить так.
     public static IDetailContext<TPayload, TState> Create(ChatContext context)
     {
         if (string.IsNullOrEmpty(context.Payload))
@@ -64,11 +27,5 @@ public class RecipeContext<TPayload, TState> : IDetailContext<TPayload, TState> 
         return new RecipeContext<TPayload, TState>(context, payload, new TransactionService());
     }
 
-    public IDetailContext<TPayload, TState> SaveChanges()
-    {
-        ChatContext.Payload = JsonConvert.SerializeObject(Payload);
-        WasChanged = true;
-
-        return this;
-    }
+    
 }
--- EasyTgBot/Abstract/IDetailContext.cs
+++ EasyTgBot/Abstract/IDetailContext.cs
@@ -1,23 +1,62 @@
 using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
 using EasyTgBot.Entity;
+using Newtonsoft.Json;
 
 namespace EasyTgBot.Abstract;
 
-public interface IDetailContext<TPayload, out TState> where TState : Enum
+public abstract class IDetailContext<TPayload, TState> where TState : Enum
 {
-    public TPayload Payload { get; }
-    public IDetailContext<TPayload, TState> PostPayload(TPayload payload);
-    public IDetailContext<TPayload, TState> SaveChanges();
-    public IDetailContext<TPayload, TState> NextState();
-
-    public bool WasChanged { get; }
-    public TState State { get; }
-
-
-    public static IDetailContext<TPayload, TState> ToIDetailContext<TPayload, TState>(ChatContext context) where TState : Enum
+    private TPayload? _payload;
+    private readonly ITransactionService _transactionService;
+    public TPayload Payload
     {
-        return 
+        get
+        {
+            if (_payload == null)
+                throw new NullReferenceException($"Payload in {typeof(TPayload)} is null");
+
+            return _payload;
+        }
+        protected set => _payload = value;
     }
-    
-    
+
+    public IDetailContext<TPayload, TState> SaveChanges()
+    {
+        ChatContext.Payload = JsonConvert.SerializeObject(Payload);
+        WasChanged = true;
+
+        return this;
+    }
+    protected IDetailContext(ChatContext chatContext, TPayload? payload,
+        ITransactionService transactionService)
+    {
+        ChatContext = chatContext;
+        Payload = payload;
+        _transactionService = transactionService;
+    }
+    public IDetailContext<TPayload, TState> NextState()
+    {
+        _transactionService.NextState(ChatContext);
+        WasChanged = true;
+
+        return this;
+    }
+    public IDetailContext<TPayload, TState> PostPayload(TPayload payload)
+    {
+        Payload = payload;
+        ChatContext.Payload = JsonConvert.SerializeObject(payload);
+
+        return this;
+    }
+
+    public TState State => (TState)(object)ChatContext.State;
+    public bool WasChanged { get; private set; }
+
+    private ChatContext ChatContext { get; }
+
+    private IDetailContext(ChatContext chatContext)
+    {
+        ChatContext = chatContext;
+    }
 }
