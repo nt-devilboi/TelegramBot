8d2fdd44d8e4a23d96e71f3e0383158065042a78
devilboi95 <turnickii.n@gmail>
2025-11-04 13:24:05 +0500

    Add

---
+++ CookingBot/Application/Flows/Friends/InContext/SwitchFriends.cs
@@ -0,0 +1,35 @@
+using EasyTgBot.Abstract;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace CookingBot.Application.Flows.Friends.InContext;
+
+public class SwitchFriends(ITelegramBotClient botClient)
+	: ContextHandler<BasePayload, FriendsContext>
+{
+	private static (string addFriend, string checkRecipes) Buttons = ("Добавить друга", "Посмотреть рецепты");
+
+	protected override async Task Handle(Update update, DetailContext<BasePayload, FriendsContext> context)
+	{
+		if (update.Message?.Text == Buttons.addFriend)
+		{
+			context.State.GoTo(FriendsContext.AddFriend);
+			return;
+		}
+
+		if (update.Message?.Text == Buttons.checkRecipes)
+		{
+			context.State.GoTo(FriendsContext.CheckRecipe);
+			return;
+		}
+	}
+
+	protected override async Task Enter(DetailContext<BasePayload, FriendsContext> context)
+	{
+		await botClient.SendTextMessageAsync(context.ChatId, "Что хочешь сделать?",
+			replyMarkup: new ReplyKeyboardMarkup([Buttons.addFriend, Buttons.checkRecipes]));
+	}
+}
+
+
